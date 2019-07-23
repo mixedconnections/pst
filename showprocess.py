8c2b75c90d4c8aa64e8e3dc270fc92276c30b9fc
Mike Accardo <accardo@cpan.org>
2019-07-23 13:28:43 -0400

    * Added author,copyright,license * Cleaned up documentation * Removed semicolon

--- showprocess.py
+++ showprocess.py
@@ -1,12 +1,17 @@
-#!/usr/bin/env python
-# showprocess.py, copyright, Mike Accardo
-#
-# Program for showing the hierarchy of processes on a Linux computer
-# Usage: python <program name>
-############################
+"""
+Program for showing the hierarchy of processes on a Linux computer
+"""
+
+__author__ = "Mike Accardo"
+__copyright__ = "Copyright 2019, Mike Accardo"
+__license__ = "MIT"
+
+#!/usr/bin/python
+
+# imports
 import trees as tr 
 
-sys_command = 'ps -e l';
+sys_command = 'ps -e l'
 
 column_header, process_rows  = tr.get_processes(sys_command)
 
@@ -18,8 +23,6 @@ row_indexes = tr.get_indexes(all_headers)
 process_info = tr.extract_row_data( row_indexes, process_rows )
 
 # We have all the essential information that we need. Time to build the process trees.
-#################################################################
-
 process_tree = tr.build_process_trees( process_info )
 
 tr.print_process_trees( process_info, process_tree )
