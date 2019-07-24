9caa2b0416eb3adaf2893ba1f6571c8f4c17929b
Mike Accardo <accardo@cpan.org>
2019-07-24 13:22:33 -0400

    * Updated with proper var names

--- showprocess.py
+++ showprocess.py
@@ -9,18 +9,17 @@ __license__ = "MIT"
 #!/usr/bin/python
 
 # imports
-import trees as tr 
+import processparser as pp 
 
-sys_command = 'ps -e l'
-column_header, process_rows  = tr.get_processes(sys_command)
+ps_command = 'ps -e l'
+column_header, processes  = pp.get_processes(ps_command)
 
-# Find the index of the processes that we are interested in (PID,PPID,COMMAND)
-all_headers = column_header.split()
-row_indexes = tr.get_indexes(all_headers)
+# Find the index of the headings that we are interested in (PID,PPID,COMMAND)
+heading_indexes = pp.get_indexes(column_header)
 
-# Next, using the indexes, extract the row data for each process
-process_info = tr.extract_row_data( row_indexes, process_rows )
+# Next, using the indexes, extract the process data 
+process_info = pp.extract_process_data( heading_indexes, processes )
 
 # We have all the essential information that we need. Time to build the process trees.
-process_tree = tr.build_process_trees( process_info )
-tr.print_process_trees( process_info, process_tree )
+process_trees = pp.build_process_trees( process_info )
+pp.display_process_trees( process_info, process_trees )
