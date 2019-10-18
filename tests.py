0511752e0805112b60673562955981b1d388c0a8
Mike Accardo <accardo@cpan.org>
2019-10-18 19:50:53 -0400

    added test

--- tests.py
+++ tests.py
@@ -1,10 +1,18 @@
 import unittest
+import subprocess
 import os
+import re
 
 class PstTestCase(unittest.TestCase):
     """This class represents the pst test case"""
-    def test_installation_file_exist(self):
+    def test_installation_file_exists(self):
         self.assertEqual(os.path.exists("pst"),True)
+
+    def test_version_string(self):
+        output = subprocess.Popen(["pst", "-v"], 
+                          stdout=subprocess.PIPE).communicate()[0]
+        REGEX = re.compile('version')
+        self.assertTrue(REGEX.search('version'))
 
 if __name__ == "__main__":
     unittest.main()
