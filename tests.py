a630c9907925ffa1bf40a63734e8be502c44fbac
Mike Accardo <accardo@cpan.org>
2019-10-17 22:08:32 -0400

    first import

---
+++ tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class PstTestCase(unittest.TestCase):
+    """This class represents the pst test case"""
+    def test_pst(self):
+        self.assertEqual(1,1)
+
+if __name__ == "__main__":
+    unittest.main()
