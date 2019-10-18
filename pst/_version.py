df0508e8d5a79d254cdd1291dce910514fd35672
Mike Accardo <accardo@cpan.org>
2019-10-18 12:49:18 -0400

    adding file

---
+++ pst/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides pst version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update pst` to change this file.
+
+from incremental import Version
+
+__version__ = Version('pst', 19, 10, 0)
+__all__ = ["__version__"]
