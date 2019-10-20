8431b1b7ec0e533afd1d83928bbbd84b331510b5
mixedconnections <perlhero@yahoo.com>
2019-10-19 21:55:06 -0400

    Changed test name

--- tests.py
+++ tests.py
@@ -5,7 +5,7 @@ import re
 
 class PstTestCase(unittest.TestCase):
     """This class represents the pst test case"""
-    def test_installation_file_exists(self):
+    def test_file_exists(self):
         self.assertEqual(os.path.exists("pst"),True)
 
     def test_file_access(self):
