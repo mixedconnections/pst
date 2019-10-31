1641f92b5257b85e6a09f1598388dc4bcc195288
mixedconnections <mixed.connections2@gmail.com>
2019-10-31 12:12:00 -0400

    added version 1.5.0

--- pst/_version.py
+++ pst/_version.py
@@ -7,5 +7,5 @@ Provides pst version information.
 
 from incremental import Version
 
-__version__ = Version('pst', 19, 10, 0, release_candidate=1)
+__version__ = Version('pst', 1, 5, 0)
 __all__ = ["__version__"]
